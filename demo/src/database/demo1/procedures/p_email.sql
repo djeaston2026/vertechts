887fdcd6f3213cf50af8f777a5b9bef4371189b5
dave.easton <djeaston2008@gmail.com>
2026-01-19 16:41:42 +0000

    Next release

---
+++ demo/src/database/demo1/procedures/p_email.sql
@@ -0,0 +1,8 @@
+create or replace procedure demo1.p_email as
+begin
+    null;
+end;
+/
+
+
+-- sqlcl_snapshot {"hash":"bd7dc34aa0a6cb070ad93eb40888a63dcfcd9533","type":"PROCEDURE","name":"P_EMAIL","schemaName":"DEMO1","sxml":""}

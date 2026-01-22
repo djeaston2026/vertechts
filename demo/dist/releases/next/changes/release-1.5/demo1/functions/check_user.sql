64931ca8607d420b46e1a11f013bdaca79d12814
dave.easton <djeaston2008@gmail.com>
2026-01-22 16:41:32 +0000

    Add apex 101

---
+++ demo/dist/releases/next/changes/release-1.5/demo1/functions/check_user.sql
@@ -0,0 +1,27 @@
+-- liquibase formatted sql
+-- changeset DEMO1:1769012078686 stripComments:false  logicalFilePath:release-1.5/demo1/functions/check_user.sql
+-- sqlcl_snapshot demo/src/database/demo1/functions/check_user.sql:null:d9135f102de4d639c2a4623b51cfe111f85f6827:create
+
+create or replace function demo1.check_user (
+    emp_id number
+) return varchar2 as
+    v_record_count number;
+begin
+    select
+        count(*)
+    into v_record_count
+    from
+        emp
+    where
+        empno = emp_id;
+
+    return
+        case
+            when v_record_count > 0 then
+                'Y'
+            else
+                'N'
+        end;
+end;
+/
+

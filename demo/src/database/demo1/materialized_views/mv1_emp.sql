95f11a4732c5f3abcb56b1ed2008c464bcd5c0e8
dave.easton <djeaston2008@gmail.com>
2026-02-12 12:05:00 +0000

    Updated  MV

--- demo/src/database/demo1/materialized_views/mv1_emp.sql
+++ demo/src/database/demo1/materialized_views/mv1_emp.sql
@@ -1,14 +1,7 @@
 create materialized view demo1.mv1_emp (
     empno,
     ename,
-    job,
-    mgr,
-    hiredate,
-    sal,
-    comm,
-    deptno,
-    email,
-    teest2
+    job
 ) build immediate using index
     refresh force
     on demand
@@ -19,18 +12,11 @@ create materialized view demo1.mv1_emp (
     disable concurrent refresh
 as
     select
-        emp.empno    empno,
-        emp.ename    ename,
-        emp.job      job,
-        emp.mgr      mgr,
-        emp.hiredate hiredate,
-        emp.sal      sal,
-        emp.comm     comm,
-        emp.deptno   deptno,
-        emp.email    email,
-        emp.teest2   teest2
+        empno,
+        ename,
+        job
     from
-        emp emp;
+        emp;
 
 
--- sqlcl_snapshot {"hash":"d7db376ca2877bc13f24a2632684ae40c729a609","type":"MATERIALIZED_VIEW","name":"MV1_EMP","schemaName":"DEMO1","sxml":"\n  <MATERIALIZED_VIEW xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>DEMO1</SCHEMA>\n   <NAME>MV1_EMP</NAME>\n   <COL_LIST>\n      <COL_LIST_ITEM>\n         <NAME>EMPNO</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>ENAME</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>JOB</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>MGR</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>HIREDATE</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>SAL</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>COMM</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>DEPTNO</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>EMAIL</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>TEEST2</NAME>\n         \n      </COL_LIST_ITEM>\n   </COL_LIST>\n   <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n   <PHYSICAL_PROPERTIES>\n      <HEAP_TABLE></HEAP_TABLE>\n   </PHYSICAL_PROPERTIES>\n   <BUILD>IMMEDIATE</BUILD>\n   <REFRESH>\n      <WITH_PRIMARY_KEY></WITH_PRIMARY_KEY>\n      <LOCAL_ROLLBACK_SEGMENT>\n         <DEFAULT></DEFAULT>\n      </LOCAL_ROLLBACK_SEGMENT>\n      <CONSTRAINTS>ENFORCED</CONSTRAINTS>\n   </REFRESH>\n   \n   \n</MATERIALIZED_VIEW>"}
+-- sqlcl_snapshot {"hash":"697cc093f77f1efbbc58f544413b35ead5f62ef3","type":"MATERIALIZED_VIEW","name":"MV1_EMP","schemaName":"DEMO1","sxml":"\n  <MATERIALIZED_VIEW xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>DEMO1</SCHEMA>\n   <NAME>MV1_EMP</NAME>\n   <COL_LIST>\n      <COL_LIST_ITEM>\n         <NAME>EMPNO</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>ENAME</NAME>\n         \n      </COL_LIST_ITEM>\n      <COL_LIST_ITEM>\n         <NAME>JOB</NAME>\n         \n      </COL_LIST_ITEM>\n   </COL_LIST>\n   <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n   <PHYSICAL_PROPERTIES>\n      <HEAP_TABLE></HEAP_TABLE>\n   </PHYSICAL_PROPERTIES>\n   <BUILD>IMMEDIATE</BUILD>\n   <REFRESH>\n      <WITH_PRIMARY_KEY></WITH_PRIMARY_KEY>\n      <LOCAL_ROLLBACK_SEGMENT>\n         <DEFAULT></DEFAULT>\n      </LOCAL_ROLLBACK_SEGMENT>\n      <CONSTRAINTS>ENFORCED</CONSTRAINTS>\n   </REFRESH>\n   \n   \n</MATERIALIZED_VIEW>"}

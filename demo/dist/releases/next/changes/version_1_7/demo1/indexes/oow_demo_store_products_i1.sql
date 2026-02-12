16d4ce0793bd08ebebc9c8bb5395623e8ae21c45
dave.easton <djeaston2008@gmail.com>
2026-02-12 12:38:44 +0000

    Update Deployment

--- demo/dist/releases/next/changes/version_1_7/demo1/indexes/oow_demo_store_products_i1.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
--- changeset DEMO1:1770123134654 stripComments:false  logicalFilePath:version_1_7/demo1/indexes/oow_demo_store_products_i1.sql
--- sqlcl_snapshot demo/src/database/demo1/indexes/oow_demo_store_products_i1.sql:null:0710a63cf7013c3cdc73ce1e8716de3014807263:create
-
-create index demo1.oow_demo_store_products_i1 on
-    demo1.oow_demo_store_products (
-        store_id
-    );
-
--- demo/dist/releases/next/changes/version_1_7/demo1/sequences/oow_demo_seq.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql
--- changeset DEMO1:1770123142246 stripComments:false  logicalFilePath:version_1_7/demo1/sequences/oow_demo_seq.sql
--- sqlcl_snapshot demo/src/database/demo1/sequences/oow_demo_seq.sql:null:8ef43481e59f37ef664c6de26505bb25579993f0:create
-
-create sequence demo1.oow_demo_seq minvalue 1 maxvalue 9999999999999999999999999999 increment by 1 /* start with n */ cache 20 noorder
-nocycle nokeep noscale global;
-
